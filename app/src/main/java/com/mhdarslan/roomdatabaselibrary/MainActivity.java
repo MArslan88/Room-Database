fa6c8ecd8fa76c4028e629fa366f6f623ea5b6eb
MArslan88 <gery_maz_555@hotmail.com>
2022-10-22 10:04:39 +0500

    initialization of design components

--- app/src/main/java/com/mhdarslan/roomdatabaselibrary/MainActivity.java
+++ app/src/main/java/com/mhdarslan/roomdatabaselibrary/MainActivity.java
@@ -3,12 +3,21 @@ package com.mhdarslan.roomdatabaselibrary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edtTitle, edtAmount;
+    Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initialization
+        edtTitle = findViewById(R.id.edtName);
+        edtAmount = findViewById(R.id.edtAmount);
+        btnAdd = findViewById(R.id.btnAdd);
     }
 }
