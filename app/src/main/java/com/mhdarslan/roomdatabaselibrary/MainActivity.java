171b39e211f9c038d36a49ec5b50673ff0821101
MArslan88 <gery_maz_555@hotmail.com>
2022-10-23 22:07:37 +0500

    Database created, and view on LOG

--- app/src/main/java/com/mhdarslan/roomdatabaselibrary/MainActivity.java
+++ app/src/main/java/com/mhdarslan/roomdatabaselibrary/MainActivity.java
@@ -3,8 +3,12 @@ package com.mhdarslan.roomdatabaselibrary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     EditText edtTitle, edtAmount;
@@ -19,5 +23,25 @@ public class MainActivity extends AppCompatActivity {
         edtTitle = findViewById(R.id.edtName);
         edtAmount = findViewById(R.id.edtAmount);
         btnAdd = findViewById(R.id.btnAdd);
+
+        DatabaseHelper databaseHelper = DatabaseHelper.getDB(this);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = edtTitle.getText().toString();
+                String amount = edtAmount.getText().toString();
+                databaseHelper.expenseDao().addTx(
+//                        new Expense("Food","100")
+                        new Expense(title,amount)
+                );
+
+                ArrayList<Expense> arrayExpenses = (ArrayList<Expense>) databaseHelper.expenseDao().getAllExpense();
+                for(int i=0; i<arrayExpenses.size(); i++){
+                    Log.d("Data","Titile: "+arrayExpenses.get(i).getTitle() + " Amount: "+arrayExpenses.get(i).getAmount());
+                }
+            }
+        });
+
     }
 }
