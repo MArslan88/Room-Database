1ac2193500d154d8234a8ad0d66b7a0d5d16a08b
MArslan88 <gery_maz_555@hotmail.com>
2022-10-23 20:57:10 +0500

    constructors, getter and setter included

--- app/src/main/java/com/mhdarslan/roomdatabaselibrary/Expense.java
+++ app/src/main/java/com/mhdarslan/roomdatabaselibrary/Expense.java
@@ -2,6 +2,7 @@ package com.mhdarslan.roomdatabaselibrary;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "expense")  // use annotation @Entity
@@ -15,4 +16,41 @@ public class Expense {
 
     @ColumnInfo(name = "amount")  // column-3
     private String amount;
+
+    // this one is our main constructor
+    Expense(int id, String title, String amount){
+        this.id =  id;
+        this.title = title;
+        this.amount = amount;
+    }
+
+    @Ignore  // because this one is not our main constructor
+    Expense(String title, String amount){
+        this.title = title;
+        this.amount = amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
 }
